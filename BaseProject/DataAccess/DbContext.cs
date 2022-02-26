a9e4abedc5b4441bfc2e7bb51901d1e2296035cd
msyilmazOfficial <msyilmaz.official@gmail.com>
2022-02-26 18:10:03 +0300

    new

--- BaseProject/DataAccess/DbContext.cs
+++ BaseProject/DataAccess/DbContext.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace DataAccess
 {
-    public class DbContext: Microsoft.EntityFrameworkCore.DbContext
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("data source=DESKTOP-JTHUOM0\\MSSQLSERVER1; Initial Catalog = MY_PROJECT; Integrated Security=True;");
+        }
 
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ BaseProject/DataAccess/Migrations/20220226145920_initialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class initialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    LogInId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    LogInPassword = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
