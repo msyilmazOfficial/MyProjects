483dd6c536c40a3d1360136346e5925437250671
msyilmazOfficial <msyilmaz.official@gmail.com>
2022-10-22 15:10:11 +0300

    API

---
+++ BaseProject/API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Business.Abstract;
+using Business.Concrete;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService userService;
+
+        public UsersController()
+        {
+            userService = new UserManager();
+        }
+
+        [HttpGet]
+        public List<User> get()
+        {
+            return userService.GetUsers();
+        }
+        [HttpGet("{id}")]
+        public User GetUser(int id)
+        {
+            return userService.GetUserById(id);
+        }
+    }
+}
--- BaseProject/Business/Abstract/IUserService.cs
+++ BaseProject/Business/Abstract/IUserService.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Business.Abstract
 {
-    internal interface IUserService
+    public interface IUserService
     {
         List<User> GetUsers();
 
